5b450aa15ed0833295a183ee0a52fd8c6db86f18
Lada Tochtcheva <lada.toch@gmail.com>
2017-04-30 00:30:49 -0400

    create: add recipe input form skeleton

--- browser/components/AddRecipe.js
+++ browser/components/AddRecipe.js
@@ -6,8 +6,21 @@ export default class AddRecipe extends React.Component {
 
     return (
       <div>
-        Add Recipe Component
+        <h1>Add New Recipe</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="formGroupExampleInput">Example label</label>
+            <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="formGroupExampleInput2">Another label</label>
+            <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input"/>
+          </div>
+        </form>
+
       </div>
+
+
     )
   }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,49 @@
 const express = require('express');
 const router = express.Router();
-
+const Recipe = require('../models/recipe');
 module.exports = router;
+
+router.get('/recipes', function(req, res, next) {
+  Recipe.findAll({})
+    .then(function (foundRecipes) {
+            res.json(foundRecipes);
+         })
+    .catch(next);
+});
+
+router.get('/recipes/:id', function(req, res, next) {
+  Recipe.findOne({
+    where: { id: req.params.id}
+  }).
+  then(function (theRecipe) {
+    if (!theRecipe) {
+      res.sendStatus(404);
+    }
+    else {res.json(theRecipe);}
+  })
+  .catch(next);
+});
+
+router.post('/recipes', function(req, res, next) {
+    Recipe.create(req.body)
+    .then(function (newRecipe) {
+       res.json(newRecipe);
+  })
+    .catch(next);
+});
+
+router.put('/recipes/:id', function(req, res, next) {
+      Recipe.update(req.body,
+         {
+          where: {
+              id: req.params.id,
+          },
+          returning: true
+         })
+      .then(function (updatedRecipe) {
+         res.json(updatedRecipe[1][0]);
+       })
+      .catch(next);
+     }
+  )
+
